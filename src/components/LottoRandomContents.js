7891b01870c7fdeee68716117ddb4f874dec6246
cys779988 <cys779988@naver.com>
2021-03-19 16:46:23 +0900

    commit

---
+++ src/components/LottoRandomContents.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import "./LottoRandomContents.css";
+import LottoBoxComponent from './LottoBoxComponent';
+
+class LottoRandomContents extends Component {
+    render(){
+
+        const {lottoNumbers} = this.props;
+        
+        return(
+            <div className="lotto-random-contents">
+                {lottoNumbers.map((lottoNumber, i) => 
+                <LottoBoxComponent
+                key={i}
+                lottoNumber={lottoNumber}
+                />)}
+                
+
+            </div>
+        );
+    }
+}
+export default LottoRandomContents;
